112501938219b7c6fbe2257700d38430dd0e2c78
aSzenos <matyi.juhasz12@gmail.com>
2026-01-02 16:01:52 +0100

    Added JS, header and footer

---
+++ script.js
@@ -0,0 +1,24 @@
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+function openLink(url, newTab = false) {
+    if (newTab) {
+        window.open(url, '_blank');
+    } else {
+        window.location.href = url;
+    }
+}
+function handleNav(event, sectionId) {
+    const isIndex =
+        window.location.pathname.endsWith('index.html') ||
+        window.location.pathname === '/' ||
+        window.location.pathname === '';
+
+    if (isIndex) {
+        event.preventDefault();
+        scrollToSection(sectionId);
+    }
+}
